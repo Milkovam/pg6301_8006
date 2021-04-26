956a39014f5857777b74d4999ddd5a5063d7e33f
Milkovam <makke.milkova@gmail.com>
2021-04-26 22:14:22 +0200

    created messagingsystem localt, able to send messages

--- src/client/MessagingSystem.jsx
+++ src/client/MessagingSystem.jsx
@@ -1,9 +1,18 @@
 import * as React from "react";
 import {useLoading} from "./useLoading";
+import {useState} from "react";
 
 export function MessagingSystem({loadMessaging}) {
+    const [messageFelt,setMessageFelt] =useState([]);
     const {loading, error} = useLoading(async () => await loadMessaging());
+    const [message, setMessage] =useState("");
 
+    function handleSubmitMessage(e) {
+        e.preventDefault();
+        setMessageFelt ([... messageFelt,message]);
+        setMessage("");
+
+    }
     if (loading) {
         return <div>...Loading...</div>;
     }
@@ -16,24 +25,27 @@ export function MessagingSystem({loadMessaging}) {
     }
     return (
 
-       <>
-           <header><h1>Welcome to chat app</h1> </header>
-           <main> <div id="messageFelt">
-
-           </div>
-           </main>
-           <footer>
-               <form>
-                   <input type="text" autoFocus={true}/>
-                   <button>Submit</button>
-               </form>
-           </footer>
-           </>
+        <>
+            <header><h1>Welcome to chat app</h1> </header>
+            <main> <div id="messageFelt">
+                {messageFelt.map((message, index) => (
+                    <div key={index}> {message}</div>
+                ))}
+            </div>
+            </main>
+            <footer>
+                <form onSubmit ={handleSubmitMessage}>
+                    <input type="text"
+                           autoFocus={true}
+                           value={message}
+                           onChange={(e)=>setMessage(e.target.value)}/>
+                    <button>Submit</button>
+                </form>
+            </footer>
+        </>
 
     );
 }
-
-
 
 
 
