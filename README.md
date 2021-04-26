84a1fdbda46a5a3d9766c2e60890cb61203c2d07
Milkovam <makke.milkova@gmail.com>
2021-04-26 12:46:25 +0200

    Created an authorization URL. Used cloud credentinals to get client id and secret, as well as redirecting to authorization_endpoint

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Starting small react application 
+done with routing profile,login and callback 
+call get with access token
--- src/client/Application.jsx
+++ src/client/Application.jsx
@@ -1,13 +1,20 @@
 import * as React from "react";
+import {useState} from "react";
 import {BrowserRouter, Link} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {ProfilePage} from "./ProfilePage";
 import {fetchJSON} from "./http";
-import {useState} from "react";
+import {LoginPage} from "./LoginPage";
 
 
 export function Application(){
     const [access_token,setAccess_token]=useState();
+
+    const googleIdentityProvider = {
+        discoveryURL: "https://accounts.google.com/.well-known/openid-configuration",
+        client_id: "790682006731-tedr7a164klngk19hf078gceu6d17klo.apps.googleusercontent.com",
+
+    };
 
     function loadProfile() {
         return fetchJSON ("/api/profile", {
@@ -28,10 +35,10 @@ export function Application(){
                    </ul>
                </Route>
                <Route path={"/profile"}>
-                   <ProfilePage loadProfile={loadProfile} />
+                   <ProfilePage loadProfile={loadProfile}/>
                </Route>
                <Route path={"/login"} exact>
-                   <h1>Login</h1>
+                   <LoginPage identityProvider={googleIdentityProvider}/>
                </Route>
                <Route path={"/login/callback"}>
                    <h1>Login callback</h1>
