d3470ab7d61ab07830b0e1978b7ef6a83d261a78
Milkovam <makke.milkova@gmail.com>
2021-04-26 12:09:11 +0200

    ProfilePage gets profile from api.

---
+++ src/client/ProfilePage.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import {useLoading} from "./useLoading";
+
+export function ProfilePage({loadProfile}) {
+    const {loading,error,data} =useLoading(async() => await loadProfile());
+
+    if (loading){
+        return <div>...Loading...</div>;
+    }
+    if(error){
+        return(
+            <div><h1>Ops, some error occurred</h1>
+            <div>{error.toString()}</div>
+        </div>
+    );
+    }
+    return(
+        <div> <h1>Profile</h1>
+    <div>{data.username}
+    </div>
+        </div>
+    );}
