108042c3e1bc0cfdec0fbc125c59334d9c0799af
Milkovam <makke.milkova@gmail.com>
2021-04-27 12:19:34 +0200

    sidebar added;

--- src/client/MessagingSystem.jsx
+++ src/client/MessagingSystem.jsx
@@ -2,25 +2,28 @@ import * as React from "react";
 import {useLoading} from "./useLoading";
 import {useState} from "react";
 import {useEffect} from "react";
+import { Link} from "react-router-dom";
+
 
 export function MessagingSystem({loadMessaging}) {
 
-    const [messageFelt,setMessageFelt] =useState([]);
+    const [messageFelt, setMessageFelt] = useState([]);
+
     const {loading, error} = useLoading(async () => await loadMessaging());
-    const [message, setMessage] =useState("");
-    const [ws,setWs] =useState();
+    const [message, setMessage] = useState("");
+    const [ws, setWs] = useState();
 
 
     useEffect(() => {
-        const ws =new WebSocket("ws://localhost:3000");
+        const ws = new WebSocket("ws://localhost:3000");
         ws.onopen = (event) => {
             console.log("opened", event);
         };
         ws.onmessage = (event) => {
             console.log("from server", event);
-            setMessageFelt ((messageFelt) =>[... messageFelt,event.data]);
+            setMessageFelt((messageFelt) => [...messageFelt, event.data]);
         };
-        ws.onclose =(event) => {
+        ws.onclose = (event) => {
             console.log("close", event);
         };
         setWs(ws);
@@ -32,6 +35,7 @@ export function MessagingSystem({loadMessaging}) {
         setMessage("");
 
     }
+
     if (loading) {
         return <div>...Loading...</div>;
     }
@@ -45,26 +49,39 @@ export function MessagingSystem({loadMessaging}) {
     return (
 
         <>
-            <header><h1>Welcome to chat app</h1> </header>
-            <main> <div id="messageFelt">
-                {messageFelt.map((message, index) => (
-                    <div key={index}> {message}</div>
-                ))}
-            </div>
+            <header><h1>Welcome to our messaging system</h1>
+                <Link to={"/"}>Home </Link>
+                <div id ="sidebar">
+
+                        <Link to="/add" className="btn btn-secondary"> Add user</Link>
+                        <Link to={"/users"} className="btn btn-secondary"> Display users</Link>
+                     </div>
+            </header>
+
+
+
+            <main>
+                <div id="messageFelt">
+                    {messageFelt.map((message, index) => (
+                        <div key={index}> {message}</div>
+                    ))}
+                </div>
             </main>
             <footer>
-                <form onSubmit ={handleSubmitMessage}>
-                    <input type="text"
+                <form onSubmit={handleSubmitMessage}>
+                    <input type="text" placeholder="Type your message here"
                            autoFocus={true}
                            value={message}
-                           onChange={(e)=>setMessage(e.target.value)}/>
-                    <button>Submit</button>
+                           onChange={(e) => setMessage(e.target.value)}/>
+                    <button id="sendMsg">Send message</button>
                 </form>
             </footer>
-        </>
 
-    );
-}
+   </>
+    ); }
+
+
+
 
 
 
